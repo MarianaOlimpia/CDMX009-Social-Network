ad70728fbc6dd9ce0f2bddf73c66eb133a959239
unknown <alitasang@gmail.com>
2020-04-29 07:33:07 -0500

    merge

---
+++ src/Views/post.js
@@ -0,0 +1,26 @@
+export default () => {
+    const postView = `
+              <form class="registerForm">
+                  <input type="email" required id="loginMail" placeholder= "Correo electrónico">
+                  <input type="password" required id="loginPassword" minlength="6" placeholder="Contraseña">
+                  <button type="submit" class="registerButton" id="loginButton">INICIAR SESIÓN</button>
+                  <p><a href="#/passwordRecovery" id="recover">¿Has olvidado la contraseña?</a></p>
+              </form> 
+              <div class="or"><h3>o</h3></div>
+              <div class="conectWithSocialMedia">
+                  <input type="image" src="images/facebook.PNG" id="facebookLogin" class="buttons"></button>
+                  <input type="image" src="images/Google.PNG" id="googleLogin" class="buttons"></button>
+                  <p>¿No tienes cuenta?<a href="#/register">Registrate</a></p>
+              </div>  `;
+    const views = document.createElement('div');
+    views.setAttribute('class', 'viewsStyle');
+    views.innerHTML = postView;
+  
+    // const loginButton = views.querySelector('#loginButton');
+    // const passRec = views.querySelector('#recover');
+    const facebookLogin = views.querySelector('#facebookLogin');
+    const googleLogin = views.querySelector('#googleLogin');
+    googleLogin.addEventListener('click', loginFunctions.google);
+    facebookLogin.addEventListener('click', loginFunctions.facebook);
+    return views;
+  };
